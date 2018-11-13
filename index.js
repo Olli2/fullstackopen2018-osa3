478c51a0bb6e679ce1e7410de9d74ebf16ee21a3
Olli Eskola <olli.eskola@siili.com>
2018-11-13 14:30:55 +0200

    tehtävät 3.13 - 3.14

--- index.js
+++ index.js
@@ -7,43 +7,22 @@ app.use(morgan('tiny'))
 const cors = require('cors')
 app.use(cors())
 app.use(express.static('build'))
-
-
-  let persons = [
-    {
-        "name": "Arto Hellas",
-        "number": "040-123456",
-        "id": 1
-    },
-    {
-        "name": "Martti Tienari",
-        "number": "040-123456",
-        "id": 2
-    },
-    {
-        "name": "Arto Järvinen",
-        "number": "040-123456",
-        "id": 3
-    },
-    {
-        "name": "Lea Kutvonen",
-        "number": "040-123456",
-        "id": 4
-    },
-  ]
-
+const Person = require('./models/person')
 
   app.get('/api/persons', (req, res) => {
-      res.json(persons)
+      Person
+        .find({})
+        .then(persons => {
+            res.json(persons.map(Person.format))
+        })
   })
 
   app.get('/api/persons/:id', (req, res) => {
-    const person = persons.find(a => a.id === Number(req.params.id))
-    if(person) {
-       res.json(person) 
-    } else {
-        res.status(404).end()
-    }
+    Person
+        .findById(Number(req.params.id))
+        .then(a => {
+            res.json(a)
+        })
   })
 
   app.get('/info', (req, res) => {
@@ -53,28 +32,20 @@ app.use(express.static('build'))
   app.post('/api/persons/', (req, res) => {
       const genId = Math.random() * (50000 - 5) + 5
       const body = req.body
-
-      if(body.number === undefined || body.number === "") {
-          return res.status(404).json({error: 'invalid number in POST request'})
+      if (body.content === undefined) {
+        return res.status(400).json({error: 'content missing'})
       }
-      if(body.name === undefined || body.name === "") {
-        return res.status(404).json({error: 'invalid name in POST request'})
-      } else if (persons.find(person => person.name === body.name)) {
-        return res.status(404).json({error: 'name must be LÉ UNIQUE!'})
-      }
-
-      const person = {
-          name: body.name,
-          number: body.number,
-          id: genId,
-      }
-
-      persons.push(person)
-      res.json(person)
+      const person = new Person({
+        name: body.content.name,
+        number: body.content.number
+      })
+      person.save()
+        .then(savedPerson => {
+            res.json(savedPerson)
+        })
   })
 
   app.delete('/api/persons/:id', (req, res) => {
-    console.log('DELETE /api/persons/'+req.params.id)
     persons = persons.filter(person => person.id !== Number(req.params.id))
     res.status(204).end()
   })
---
+++ models/person.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+const url = 'mongodb://user:password@ds155663.mlab.com:55663/fullsdb'
+mongoose.connect(url, { useNewUrlParser: true })
+
+var personSchema = mongoose.Schema({
+    name: String,
+    number: String
+})
+
+personSchema.statics.format = function(person) {
+    return {name: person.name, number: person.number}
+}
+
+const Person = mongoose.model('Person', personSchema)
+
+module.exports = Person
